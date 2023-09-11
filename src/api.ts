6311a4976aab98bb748477e889cec5c362a9dcf8
John Dunning <fw@johndunning.com>
2023-09-10 18:30:32 -0700

    Return errors from receive() as type: "error"
    Pass the stringified error back in `errorJSON` field.
    Add handleError() to process the message dispatched from handleMessage().
    Add a generic type to receive() and ReceiverFn.

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,32 @@
 import { DeferredPromise } from "./DeferredPromise";
 
-export type ReceiverFn = (...data: any) => any;
+export type ReceiverFn<R> = (...data: any) => R;
 
-interface Message {
-	type: "call"|"response";
+type MessageBase = {
+	type: "call" | "response" | "error";
 	id: number;
 	name: string;
+}
+
+type CallMessage = MessageBase & {
+	type: "call";
 	data: any;
 }
 
+type ResponseMessage = MessageBase & {
+	type: "response";
+	data: any;
+}
+
+type ErrorMessage = MessageBase & {
+	type: "error";
+	errorJSON: string;
+}
+
+type Message = CallMessage | ResponseMessage | ErrorMessage;
+
 const promisesByID: Record<number, DeferredPromise<any>> = {};
-const receiversByName: Record<string, ReceiverFn> = {};
+const receiversByName: Record<string, ReceiverFn<unknown>> = {};
 let currentID = 0;
 let post: (message: Message) => void;
 
@@ -43,19 +59,19 @@ export function call<T>(
  * whatever parameters are passed to the `call()` function.  Its return value
  * will be sent back to the caller.
  *
- * If the receiver returns a `Promise`, then no response will be sent to the
+ * If the receiver returns a promise, then no response will be sent to the
  * caller until the promise resolves.
  *
  * Only a single function can listen for any given name, so subsequent calls to
- * `receive()` will replace previous functions.
+ * `receive()` will replace the previously registered function.
  *
  * @param {string} name - The name of the receiver.
  * @param {ReceiverFn} fn - The function that will receive calls to the `name`
  * parameter.
  */
-export function receive(
+export function receive<R>(
 	name: string,
-	fn: ReceiverFn)
+	fn: ReceiverFn<R>)
 {
 	receiversByName[name] = fn;
 }
@@ -80,56 +96,58 @@ if (typeof window === "undefined") {
 } else {
 	post = (message: Message) => window.parent.postMessage({ pluginMessage: message }, "*");
 
-	addEventListener("message", (event) => {
-		if (typeof event.data.pluginMessage !== "undefined") {
-			handleMessage(event.data.pluginMessage);
+	addEventListener("message", ({ data: { pluginMessage } }) => {
+		if (pluginMessage && typeof pluginMessage === "object") {
+			return handleMessage(pluginMessage);
 		}
 	});
 }
 
 async function handleCall(
-	message: Message)
+	message: CallMessage)
 {
 	const { id, name, data } = message;
 	const receiver = receiversByName[name];
-	let success = true;
-	let response;
 
 	try {
-		response = await receiver(...data);
+		const response = await receiver(...data);
+
+		post({ type: "response", id, name, data: response });
 	} catch (error) {
 			// the Figma postMessage() seems to just stringify everything, but that
 			// turns an Error into {}.  so explicitly walk its own properties and
 			// stringify that.
-		response = JSON.stringify(error, Object.getOwnPropertyNames(error));
-		success = false;
-	}
+		const errorJSON = JSON.stringify(error, Object.getOwnPropertyNames(error));
 
-	post({ type: "response", id, name, data: { success, response } });
+		post({ type: "error", id, name, errorJSON });
+	}
 }
 
-async function handleResponse(
-	message: Message)
+function handleResponse(
+	message: ResponseMessage)
 {
-	const { id, data: { success, response} } = message;
+	const { id, data } = message;
 	const promise = promisesByID[id];
 
-	if (success) {
-		promise.resolve(response);
-	} else {
-			// since this is a failure, we know response is JSON, so parse it, turn it
-			// back into an Error, and reject the promise with it
-		const { message, ...rest } = JSON.parse(response);
-
-			// passing a cause to the constructor is available in Chrome 93+
-			//@ts-ignore
-		const error = new Error(message, { cause: rest });
-
-		promise.reject(error);
-	}
+	promise.resolve(data);
 }
 
-function handleMessage(
+function handleError(
+	message: ErrorMessage)
+{
+	const { id, errorJSON } = message;
+	const promise = promisesByID[id];
+		// parse the stringified error, turn it back into an Error, and reject the
+		// promise with it
+	const { message: errorMessage, ...rest } = JSON.parse(errorJSON);
+		// passing a cause to the constructor is available in Chrome 93+
+		//@ts-ignore
+	const error = new Error(errorMessage, { cause: rest });
+
+	promise.reject(error);
+}
+
+async function handleMessage(
 	message: Message)
 {
 	if (!message || typeof message !== "object") {
@@ -138,9 +156,23 @@ function handleMessage(
 
 	const { type, id, name } = message;
 
-	if (type === "call" && name in receiversByName) {
-		return handleCall(message);
-	} else if (type === "response" && id in promisesByID) {
-		return handleResponse(message);
+	switch (type) {
+		case "call":
+			if (name in receiversByName) {
+				await handleCall(message);
+			}
+			break;
+
+		case "response":
+			if (id in promisesByID) {
+				handleResponse(message);
+			}
+			break;
+
+		case "error":
+			if (id in promisesByID) {
+				handleError(message);
+			}
+			break;
 	}
 }
